5f22f062c2199136067f8d55e7cb68d98e032f50
1mends <gformosofso@gmail.com>
2022-10-26 11:59:21 -0300

    Conversor de moedas

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const button = document.getElementById('convert-button')
+
+const select = document.getElementById('currency-select')
+
+
+const dolar = 5.2
+const euro = 5.9
+
+const convertValues = () => {
+    const inputreal = document.getElementById('input-real').value
+    const realvalue = document.getElementById('real-value')
+    const dolarvalue = document.getElementById('dolar-value')
+
+    realvalue.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(inputreal);
+
+    if (select.value === 'US$ Dólar Americano') {
+
+        dolarvalue.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputreal / dolar);
+
+    }
+
+    if (select.value === '€ Euro') {
+
+        dolarvalue.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputreal / euro);
+    }
+}
+
+
+changeCurrency = () => {
+    const currencyname = document.getElementById('currency-name')
+    const currencyimg = document.getElementById('currency-img')
+
+    if (select.value === 'US$ Dólar Americano') {
+        currencyname.innerHTML = 'Dólar Americano'
+        currencyimg.src = "./assets/estados-unidos (1) 1.png"
+    }
+
+    if (select.value === '€ Euro') {
+        currencyname.innerHTML = 'Euro'
+        currencyimg.src = "./assets/euro.png"
+    }
+
+    convertValues ()
+
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
